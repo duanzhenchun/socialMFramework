1a140ab8114c3f540f66104a1ab6d0f7286da75f
Leonardo Mendez <mleonardom@gmail.com>
2013-04-30 18:04:00 -0500

    Changes in feeds and controllers plubic

--- js/global.js
+++ js/global.js
@@ -45,9 +45,9 @@ var load_followers_following= function (typef){
 }var send_fav_action = function(shared_id,favorite){
 	var comment = $('#comment_input').attr("value"); 
 	$.ajax({  
-	    url: base_url+'applications/action/shared_id/'+shared_id+'/favorite/'+favorite, 
+	    url: base_url+'applications/favorite/shared_id/'+shared_id+'/favorite/'+favorite, 
 	    success: function() {
-	    	alert("Fav ok")  
+	    	alert("You mark the application how Favorite");
 	    }  
 	}); 
 }
@@ -59,29 +59,21 @@ var send_comment = function(shared_id){
 	$.ajax({  
 	    url: base_url+'comments/publish/shared_id/'+shared_id+'/comment/'+comment, 
 	    success: function() {
-	    	alert("Your comment has already been published")  
+	    	alert("Your comment has already been published");
 	    }  
 	}); 
-}
-var application_fav = function ( shared_id, fav, package_name){
-	$.ajax({  
-	    url: base_url+'favorites/favUnfav/shared_id/'+shared_id+'/favorite/'+fav+'/package_name/'+package_name, 
-	    success: function() {
-	    	if(fav =="1"){
-	    		alert("the application has marked as favorite")
-	    	}
-	    	if(fav =="0"){
-	    		alert("the application has Unmarked as favorite")
-	    	}  
-	    }  
-	});  
-}var initFB = function(){	FB.init({      appId      : fb_app_id,                        // App ID from the app dashboard      status     : true,                             // Check Facebook Login status      xfbml      : true                              // Look for social plugins on the page    }); 	FB.login(function(response){    	if (response.status === 'connected') {    		var accessToken = response.authResponse.accessToken;
+}var initFB = function(){	FB.init({      appId      : fb_app_id,                        // App ID from the app dashboard      status     : true,                             // Check Facebook Login status      xfbml      : true                              // Look for social plugins on the page    }); 	FB.login(function(response){    	if (response.status === 'connected') {    		var accessToken = response.authResponse.accessToken;
     		$.ajax({  			    url: base_url+'auth/facebook-login/',
 			    //data: 'access_token=BAABsfHkvuU4BAAhq7ZB43i52xtmBMIHmKiZCQuu1AjJdOqJfx3nPsejD7ZC1LpGDJHYnOuHz9hjmWFZAY9QTdk0dxuasRvZBZB6nIqwTBIOPoqSB1BKJZCDw7v9lO67Lzbiz6V3E3imniWhVb1vLJj52P7g9QZCOG115BBuscYA7JB7Q1pyt0ql62w3j2uv3f2sZD',
 			    data: 'access_token='+accessToken,
 			    type:'POST',				success: function() {
 			    }  			}); 		}else{			alert("Not yet :()");		}    },{scope: 'email,read_friendlists,publish_stream'});}
-var openFeddDetail = function(){
-	alert("hey");
+var openFeddDetail = function(i,show){
 	
+	if($('#notification_box_detail_tl'+i).css('display') == 'none'){
+		$('#notification_box_detail_tl'+i).show();
+	}
+	else{
+		$('#notification_box_detail_tl'+i).css("display", "none");
+	}
 }
